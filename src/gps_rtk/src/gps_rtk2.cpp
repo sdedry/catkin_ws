097e14ee0c68c37b7064bca76ed55364cfbdbbcf
sdedry <sim.ded@hotmail.com>
2016-11-28 09:15:13 +0100

    Update gps_rtk2.cpp

--- src/gps_rtk/src/gps_rtk2.cpp
+++ src/gps_rtk/src/gps_rtk2.cpp
@@ -83,6 +83,7 @@ int main(int argc, char *argv[])
 		gps_rtk.publish(gps_msg);
 		free(token);
 		loop_rate.sleep();
+		printf("ROS OK \n");
 		//rel_pos[0] = (ref_pos[0]-pos_data[0])*pow(10,6)*1111.6/10000;
 		//rel_pos[1] = (ref_pos[1]-pos_data[1])*pow(10,6)*767.4/10000;
 		//printf("lat : %.7f - lon : %.7f \n",pos_data[0],pos_data[1]);
