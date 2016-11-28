5cfade132074d1c08cfc7a1bad4117fd8f81c65e
sdedry <sim.ded@hotmail.com>
2016-11-28 09:59:37 +0100

    Update aa_Simon.bash

--- aa_Simon.bash
+++ aa_Simon.bash
@@ -74,6 +74,48 @@ then
     tmux send-keys "sudo -i" C-m
     tmux send-keys "source /home/pi/catkin_ws/devel/setup.bash" C-m
     tmux send-keys "rosrun gps_rtk gps_rtk2" C-m
+    
+#log files, eric's code and gps launch
+elif [ "$#" -eq 1 ] && [ "$1" == 'alllog' ]
+then
+    echo "Here we go in auto mode and gps information (log recorded): freq= 50, MaxThrottlePwm=1600, Kp=0.7, Ki=0.2, Kd=0.2"
+    sleep 2
+    tmux -2 new-session -d -s $SESSION
+    tmux new-window -t $SESSION:1 -n 'ROS'
+    tmux split-window -h
+    
+    tmux select-pane -t 0
+    tmux send-keys "source /home/pi/catkin_ws/devel/setup.bash" C-m
+    tmux send-keys "roscore" C-m
+    
+    tmux select-pane -t 1
+    tmux send-keys "sleep 5" C-m
+    tmux send-keys "source /home/pi/catkin_ws/devel/setup.bash" C-m
+    tmux send-keys "rosrun navio2_imu imu_pub 49" C-m
+    
+    tmux select-pane -t 0
+    tmux split-window -v
+    tmux send-keys "sleep 5" C-m
+    tmux send-keys "sudo -i" C-m
+    tmux send-keys "source /home/pi/catkin_ws/devel/setup.bash" C-m
+    tmux send-keys "rosrun navio2_remote remote_multiCtr 50 1600 0.7 0.2 0.2" C-m
+    
+    tmux select-pane -t 1
+    tmux split-window -v
+    tmux send-keys "source /home/pi/catkin_ws/devel/setup.bash" C-m
+    tmux send-keys "sudo ifconfig usb0 192.168.2.2" C-m
+    tmux send-keys "sleep 5" C-m
+    tmux send-keys "sudo -i" C-m
+    tmux send-keys "source /home/pi/catkin_ws/devel/setup.bash" C-m
+    tmux send-keys "rosrun gps_rtk gps_rtk2" C-m
+    
+    tmux split-window -h
+    tmux send-keys "sleep 15" C-m                                       #no needs to start earlier because 10/15 sec of calibration
+    tmux send-keys "source /home/pi/catkin_ws/devel/setup.bash" C-m
+    tmux send-keys "cd /home/pi/bagfiles" C-m
+    tmux send-keys "rosbag record -a" C-m
+    
+    
 elif [ "$#" -eq 5 ]
 then
     echo "Here we go WITHOUT log files"
@@ -94,7 +136,7 @@ then
     tmux send-keys "sudo -i" C-m
     tmux send-keys "source /home/pi/catkin_ws/devel/setup.bash" C-m
     tmux send-keys "rosrun navio2_remote remote_multiCtr $1 $2 $3 $4 $5" C-m
-    elif [ "$#" -eq 6 ] && [ "$6" == '-log' ]          #to active the writing of the log files
+elif [ "$#" -eq 6 ] && [ "$6" == '-log' ]          #to active the writing of the log files
     then
         echo "Here we go WITH log files"
         sleep 2
