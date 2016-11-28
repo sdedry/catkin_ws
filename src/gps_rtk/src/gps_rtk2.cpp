fe16d957f58038738b218647a190980c5039ce52
sdedry <sim.ded@hotmail.com>
2016-11-28 11:56:27 +0100

    Update gps_rtk2.cpp

--- src/gps_rtk/src/gps_rtk2.cpp
+++ src/gps_rtk/src/gps_rtk2.cpp
@@ -74,12 +74,14 @@ int main(int argc, char *argv[])
 			//printf("%i : %s\n",i,token);
 			if(i == 2){
 				printf("lat = %s\n",token);
-				//pos_data[0] = strtof(token,NULL);
+				pos_data[0] = strtof(token,NULL);
+				printf("lat = %f\n",pos_data[0]);
 				
 			}
 			if(i == 3){
 				printf("lon = %s\n",token);
-				//pos_data[1] = strtof(token,NULL);
+				pos_data[1] = strtof(token,NULL);
+				printf("lon = %f\n",pos_data[1]);
 			}
 			token = strtok(NULL, delim);
 			i++;
